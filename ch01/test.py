6b375d87512e28710749aa6eb1fa45680c115409
Rio Zhao <rio.zhao@gmail.com>
2022-06-29 13:04:38 +0800

    629

--- ch01/test.py
+++ ch01/test.py
@@ -1,2 +1,12 @@
-A=99
-print(A)
+# A=99
+# print(A)
+
+# seconds_per_min = 60
+# seconds_per_hour = seconds_per_min * 60
+# seconds_per_day = seconds_per_hour * 24
+# div_1 = seconds_per_day / seconds_per_hour
+# div_2 = seconds_per_day // seconds_per_hour
+# print(seconds_per_hour)
+# print(seconds_per_day)
+# print(div_1)
+# print(div_2)
