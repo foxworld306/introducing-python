04d8b39d6de2ffe3711e843d3d3b173a28d25064
Rio Zhao <rio.zhao@suez.com>
2022-07-01 21:00:27 +0800

    1

---
+++ ch05/Practice.py
@@ -0,0 +1,76 @@
+# m开头的单词首字母大写
+# song = """When an eel grabs your arm,
+#     And it causes great harm,
+#     That's - a moray!"""
+
+# print(song.replace('moray', 'Moray'))
+
+
+#使用旧格式化，分别使用'roast beef', 'ham', 'head'和'clam'插值
+# question = (
+#     "my kitty cat likes %s," % "roast beef", 
+#     "my kitty cate likes %s," % "ham", 
+#     "my kitty cat fell on his %s" % "head",
+#     "And now thinks he's a %s." % "clam"
+# )
+
+# print(question)
+
+# num = 1
+# while num <= 5:
+#     print(num)
+#     num += 1
+#     print(num)
+
+# 输入的首字母大写，按q退出
+# while True:
+#     stuff = input("string to capitalize [type q to quit]:")
+#     if stuff == "q":
+#         break
+#     print(stuff.capitalize())
+
+# 读入一个整数，如果是奇数，则计算平方；偶数则跳过。按q退出
+# while True:
+#     num = input("input a number [type q to quit]:")
+#     if num == "q":
+#         break
+#     number = int(num)
+#     if number % 2 == 0: #偶数
+#         continue
+#     print(number, "squared is:", number*number)
+
+# 查找偶数，有偶数则跳出循环，否则报错
+# numbers = [1,3,5, 6]
+# position = 0
+# while position < len(numbers):
+#     num = numbers[position]
+#     if num % 2 == 0:
+#         print('Found even number', num)
+#         break
+#     position += 1
+# else:
+#     print('No even number found')
+
+# 6.2.3 查找字符串中某个字符并计数
+# word = 'thus!'
+# length = 0
+# num = 0
+# while length < len(word):
+#     for letter in word:
+#         print(letter)
+#         length += 1
+#         if letter == 'o':
+#             num += 1
+#     print('there are', num, 'O')
+
+# range() & list
+# for X in range(0, 10, -1):
+#     print(X)
+# x = list(range(0,3))
+# print(x)
+
+# 7.1.1 create a tuple
+# empty_tuple = ()
+# print(empty_tuple)
+one_ele = ('come',)
+print(one_ele)
